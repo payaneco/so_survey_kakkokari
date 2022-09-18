da35e08b4d61fe3e9c8761a527d4cc36e474086a
payaneco <6695369+payaneco@users.noreply.github.com>
2022-09-18 14:37:54 +0900

    restore

--- index.py
+++ index.py
@@ -10,12 +10,7 @@ def post_answer(dic_answers):
         db = client["so-survey"]
         collection = db["answer"]
         collection.insert_one(dic_answers)
-
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-st.write(s.getsockname()[0])
-
+        
 dic_answers = {"userid":"",
                "goodness":"",
                "wanted":"",
